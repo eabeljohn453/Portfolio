30bca74580111011f4aece92f5ecfc49ea8b73ed
eabeljohn453 <eabeljohn453@gmail.com>
2025-11-02 00:59:44 +0530

    added image

--- src/components/body/index.jsx
+++ src/components/body/index.jsx
@@ -5,21 +5,21 @@ export default function Body() {
     return (
         <div className=" flex flex-col  items-center    w-full h-[800px] bg-black">
             <div className="flex ">
-                <Image src="/assets/photo/arrow.svg" width={100} height={100} alt="Body Image" className=" bg-black ml-120 mt-14 " />
+                <Image src="/assets/photo/arrows.svg" alt="Body Image"  width={100} height={100} className=" bg-black ml-120 mt-14 " />
                 <h1 className="ml-5 mt-10 text-[24px] font-bold ">Hello! I Am  <span className="text-[30px] bg-linear-to-r from-purple-700 to-pink-500 bg-clip-text text-transparent">
                     Eabel John
-                </span></h1></div> 
-      <div className="relative w-[220px] h-[220px] rounded-full p-1 bg-linear-to-tr from-[#ff00cc] via-[#a100ff] to-[#00d4ff]">
-        <div className="w-full h-full rounded-full bg-[#0D0D0D] p-1">
-          <Image
-            src="/assets/photo/logo.jpeg"
-            alt="Profile"
-            width={300}
-            height={200}
-            className="rounded-full object-cover  "
-          /> 
-      </div>
-    </div>
+                </span></h1></div>
+            <div className="relative w-[220px] h-[220px] rounded-full p-1 bg-linear-to-tr from-[#ff00cc] via-[#a100ff] to-[#00d4ff]">
+                <div className="w-full h-full rounded-full bg-[#0D0D0D] p-1">
+                    <Image
+                        src="/assets/photo/logo.jpeg"
+                        alt="Profile"
+                        width={300}
+                        height={200}
+                        className="rounded-full object-cover  "
+                    />
+                </div>
+            </div>
             <h1 className="text-[50px] font-bold leading-tight mt-10 text-center">
                 I do <span className="bg-linear-to-r from-[#FF8C42] via-[#FF4FC7] to-[#A44CFF] bg-clip-text text-transparent font-extrabold">
                     CODE
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer(){
     return(
         <div className="flex justify-center gap-20  items-center w-full h-20 bg-black">
-            <Link href="/home">
+            <Link href="/">
            <h1 className="relative h-14 flex justify-center items-center text-[20px] font-bold  cursor-pointer transition-all duration-300 w-26 text-center rounded-xl z-10
     after:content-[''] after:absolute after:bottom-2 after:left-0 after:h-[3px] after:w-0 after:bg-linear-to-r after:from-[#8010B7] after:to-[#AC21B1] after:transition-all after:duration-500 hover:after:w-full text-white">
        Home</h1></Link>
