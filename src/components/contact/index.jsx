fd15c63095b0a0da2fa787efbe120356a6d8c794
eabeljohn453 <eabeljohn453@gmail.com>
2025-11-02 10:38:14 +0530

    port changed

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
     showToast("📤 Sending...", "yellow");
 
     try {
-      const res = await fetch("http://52.62.35.240:5002/contact", {
+      const res = await fetch("http://52.62.35.240/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, phno, email, message }),
